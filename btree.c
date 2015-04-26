cee749a43ec53ca323d25b621a949dafb214245e
cj <cj@debian>
2015-04-26 23:55:11 +0800

    添加了删除算法，花了一天，除了一个错误一直调试

--- btree.c
+++ btree.c
@@ -147,10 +147,238 @@ btree_insert(struct btree_s *tree, int k)
 	btree_insert_nonfull(tree, tree->root, k);
 }
 
+
+void 
+left_rotate(struct btree_s *tree, struct bnode_s *r, int key_pos)
+{
+
+	struct bnode_s *right_node = r->child[RIGHT_CHILD_OF_KEY(key_pos)];
+	struct bnode_s *left_node = r->child[LEFT_CHILD_OF_KEY(key_pos)];
+
+	/* 修改左节点 */	
+	left_node->nkeys++;
+	left_node->key[left_node->nkeys-1] = r->key[key_pos];
+	left_node->child[left_node->nkeys] = right_node->child[0];
+
+	/* 修改父节点 */
+	r->key[key_pos] = right_node->key[0];
+
+	/* 修改右节点 */
+	right_node->nkeys--;
+	int i;
+	for (i = 0; i < right_node->nkeys; i++) {
+		right_node->key[i] = right_node->key[i+1];
+	}
+	for (i = 0; i < right_node->nkeys+1; i++) {
+		right_node->child[i] = right_node->child[i+1];
+	}
+
+}
+
+void 
+right_rotate(struct btree_s *tree, struct bnode_s *r, int key_pos)
+{
+
+	struct bnode_s *right_node = r->child[RIGHT_CHILD_OF_KEY(key_pos)];
+	struct bnode_s *left_node = r->child[LEFT_CHILD_OF_KEY(key_pos)];
+
+	/* 修改右节点 */	
+	int i;
+	for (i = right_node->nkeys-1; i >= 0; i--) {
+		right_node->key[i+1] = right_node->key[i];
+	}
+	right_node->key[0] = r->key[key_pos];
+
+	for (i = right_node->nkeys; i >= 0; i--) {
+		right_node->child[i+1] = right_node->child[i];
+	}
+	right_node->child[0] = left_node->child[left_node->nkeys];
+	right_node->nkeys++;
+
+	/* 修改父节点 */
+	r->key[key_pos] = left_node->key[left_node->nkeys-1];
+
+	/* 修改左节点 */
+	left_node->nkeys--;
+
+}
+
+void 
+merge_child(struct btree_s *tree, struct bnode_s *r, int key_pos)
+{
+	struct bnode_s *left_node = r->child[LEFT_CHILD_OF_KEY(key_pos)];
+	struct bnode_s *right_node = r->child[RIGHT_CHILD_OF_KEY(key_pos)];
+
+
+	/* 整理key */
+	int key_beg = left_node->nkeys;
+	left_node->key[key_beg] = r->key[key_pos];
+	key_beg++;
+
+
+	int i;
+	for (i = 0; i < right_node->nkeys; i++) {
+		left_node->key[key_beg+i] = right_node->key[i];
+	}	
+
+	/* 整理child */
+	int child_beg = left_node->nkeys+1;
+	for (i = 0; i < right_node->nkeys+1; i++) {
+		left_node->child[child_beg+i] = right_node->child[i];
+	}
+
+	left_node->nkeys += 1 + right_node->nkeys;
+	/* 整理父节点key */
+	for (i = key_pos; i < r->nkeys-1; i++) { 
+		r->key[i] = r->key[i+1];
+	}	
+	/* 整理父节点child */
+	for (i = RIGHT_CHILD_OF_KEY(key_pos); i < r->nkeys; i++) {
+		r->child[i] = r->child[i+1];
+	}
+	r->nkeys--;
+}
+
+static int
+btree_delete_max(struct btree_s *tree, struct bnode_s *r)
+{
+	if (r->leaf) {
+		int max = r->key[r->nkeys-1];
+		r->nkeys--;
+		return max;
+	} else {
+		struct bnode_s *right_most_child = r->child[r->nkeys];
+		if (right_most_child->nkeys >= tree->min_key_nr+1) {
+			return btree_delete_max(tree, right_most_child);
+		} else {
+			struct bnode_s *left = r->child[r->nkeys-1];
+			if (left->nkeys >= tree->min_key_nr+1) {
+				right_rotate(tree, r, r->nkeys-1);
+			} else {
+				merge_child(tree, r, r->nkeys-1);
+			}
+			return btree_delete_max(tree, r->child[r->nkeys]);
+		}
+	}
+}
+
+static int 
+btree_delete_min(struct btree_s *tree, struct bnode_s *r)
+{
+	if (r->leaf) {
+		int min = r->key[0], i;
+		for (i = 1; i <= r->nkeys-1; i++) {
+			r->key[i-1] = r->key[i];
+		}
+		r->nkeys--;
+		return min;
+	} else {
+		struct bnode_s *left_most_chlid = r->child[0];
+		if (left_most_chlid->nkeys >= tree->min_key_nr+1) {
+			return btree_delete_min(tree, left_most_chlid);
+		} else {
+			struct bnode_s *right = r->child[1];
+			if (right->nkeys >= tree->min_key_nr+1) {
+				left_rotate(tree, r, 0);
+			} else {
+				merge_child(tree, r, 0);
+			}
+			return btree_delete_min(tree, r->child[0]);
+		}
+
+	}
+}
+
+void 
+btree_remove_impl(struct btree_s *tree, struct bnode_s *r, int k)
+{
+	int i = r->nkeys - 1;
+	while (i >= 0 && k < r->key[i])
+		i--;
+
+	if (i >= 0 && k == r->key[i]) {
+		/* 要删除的节点在本节点 */
+		if (r->leaf) {
+			/* 叶子节点，直接删除 */
+			for (; i < r->nkeys; i++) {
+				r->key[i] = r->key[i+1];
+			}
+			r->nkeys--;
+		} else {
+			/* 非叶子节点 */
+			assert(i >= 0 && "error");
+			int left = LEFT_CHILD_OF_KEY(i);
+			int right = RIGHT_CHILD_OF_KEY(i);
+			if (r->child[left]->nkeys >= tree->min_key_nr+1) {
+				/* 左边的孩子的key足够t个 */
+				r->key[i] = btree_delete_max(tree, r->child[left]);
+			} else if (r->child[right]->nkeys >= tree->min_key_nr+1) {
+				/* 右边的孩子的key足够t个 */
+				r->key[i] = btree_delete_min(tree, r->child[right]);
+			} else {
+				/* 都不够，需要合并 */
+				merge_child(tree, r, i);
+				if (r->nkeys == 0) { //?????
+					/* FIXME : free node */
+					tree->root = r->child[0];
+					btree_remove_impl(tree, tree->root, k);
+				} else {
+					btree_remove_impl(tree, r->child[i], k);
+				}
+			}
+		}
+	} else {
+		if (i >= 0 && r->leaf) {
+			printf("%d, %d, \n", i, k);
+			void btree_dump(struct btree_s *);
+			btree_dump(tree);
+			assert(0 && "not in tree");
+		}
+		/* 要删除的节点不在本节点 */
+		int remove_child_pos = RIGHT_CHILD_OF_KEY(i);
+		if (r->child[remove_child_pos]->nkeys >= tree->min_key_nr+1) {
+			/* 子节点的key个数足够t */
+			btree_remove_impl(tree, r->child[remove_child_pos], k);
+		} else {
+			/* key个数不够 */
+			if (remove_child_pos-1 >=0 && 
+					r->child[remove_child_pos-1]->nkeys >= tree->min_child_nr+1) {
+				/* 有左兄弟且左兄弟够 */
+				right_rotate(tree, r, LEFT_KEY_OF_CHILD(remove_child_pos));
+				btree_remove_impl(tree, r->child[remove_child_pos], k);
+			} else if (remove_child_pos+1 <= r->nkeys &&  
+					r->child[remove_child_pos+1]->nkeys >= tree->min_child_nr+1) {
+				/* 有右兄弟且有兄弟够 */
+				left_rotate(tree, r, RIGHT_KEY_OF_CHILD(remove_child_pos));
+				btree_remove_impl(tree, r->child[remove_child_pos], k);
+			} else {
+				/* 都不够 */
+				int merge_pos = -1; 
+				if (remove_child_pos-1 >= 0) {
+					merge_pos = LEFT_KEY_OF_CHILD(remove_child_pos);
+				} else if (remove_child_pos+1 <= r->nkeys) {
+					merge_pos = RIGHT_KEY_OF_CHILD(remove_child_pos);
+				} else {
+					printf("%d %d %d [%d]\n", remove_child_pos-1, remove_child_pos+1, r->nkeys, k);
+					assert(0 && "damn");
+				}
+				merge_child(tree, r, merge_pos);
+				if (r->nkeys == 0) {
+					/* FIXME : free node */
+					tree->root = r->child[0];
+					btree_remove_impl(tree, tree->root, k);
+				} else {
+					btree_remove_impl(tree, r->child[merge_pos], k);
+				}
+			}
+		}
+	}
+}
+
 void
 btree_remove(struct btree_s *tree, int k)
 {
-	
+	btree_remove_impl(tree, tree->root, k);
 }
 
 void 
@@ -184,6 +412,47 @@ btree_dump(struct btree_s *tree)
 }
 
 #ifdef _GTEST_
+
+static void 
+btree_check_key(struct bnode_s *n, int key_pos)
+{
+	assert(n);
+
+	struct bnode_s *left, *right;	
+	left = n->child[LEFT_CHILD_OF_KEY(key_pos)];
+	right = n->child[RIGHT_CHILD_OF_KEY(key_pos)];
+
+	assert(left && right);
+
+	int i;
+	for (i = 0; i <= left->nkeys-2; i++) {
+		assert(left->key[i] < left->key[i+1]);
+	}
+
+	assert(left->key[left->nkeys-1] < n->key[key_pos]);
+
+	assert(n->key[key_pos] < right->key[0]);
+
+	for (i = 0; i < right->nkeys-2; i++) {
+		assert(right->key[i] < right->key[i+1]);
+	}
+}
+
+static void 
+btree_check(struct bnode_s *r)
+{
+	if (!r->leaf) {
+		int i = 0;
+		for (i = 0; i < r->nkeys; i++) {
+			btree_check_key(r, i);
+		}
+
+		for (i = 0; i < r->nkeys+1; i++) {
+			btree_check(r->child[i]);
+		}
+	}
+}
+
 #include <gtest/gtest.h>
 TEST(BTree, Insert)
 {
@@ -191,12 +460,71 @@ TEST(BTree, Insert)
 	btree_init(&tree, 3);
 	int i;
 
-	for (i = 0; i < 200; i++) {
-
+	for (i = 0; i < 10; i++) {
 		btree_insert(&tree, i);
 	}
 
 	btree_dump(&tree);
+}
+
+TEST(BTree, Remove)
+{
+	struct btree_s tree;
+	btree_init(&tree, 3);
+	int i;
+
+	for (i = 0; i < 10; i++) {
+		btree_insert(&tree, i);
+	}
+
+	btree_dump(&tree);
+
+	btree_remove(&tree, 5);
+	btree_dump(&tree);
+
+	btree_remove(&tree, 9);
+	btree_dump(&tree);
+
+	btree_remove(&tree, 8);
+	btree_dump(&tree);
+}
+
+TEST(BTree, RemoveALot)
+{
+	struct btree_s tree;
+	btree_init(&tree, 3);
+	int i;
+
+	for (i = 0; i < 1000000; i++) {
+		btree_insert(&tree, i);
+	}
+
+	for (i = 10; i < 999992; i++) {
+		btree_remove(&tree, i);
+	}
+
+	btree_dump(&tree);
+}
+
+TEST(BTree, RemoveALot1)
+{
+	struct btree_s tree;
+	btree_init(&tree, 3);
+	int i;
+
+	srand(time(0));
+
+	for (i = 0; i < 100000; i++) {
+		btree_insert(&tree, i);
+	}
+
+	btree_check(tree.root);
+
+	for (i = 0; i < 99992; i += (rand() % 3 + 1) ) {
+		btree_remove(&tree, i);
+	}
+
+	btree_check(tree.root);
 }
 
 #endif
